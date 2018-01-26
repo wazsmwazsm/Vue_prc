e52730c4c1d39ba3e1050ed95c281a03b98d7601
wazsmwazsm <18748406022@163.com>
2018-01-26 20:13:10 +0800

    add vue loader sample

--- vue_route/sample/src/components/User.vue
+++ vue_route/sample/src/components/User.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="user">
     <h1>User {{ $route.params.id }}</h1>
+    <button @click="go_route(1)">1</button>
+    <button @click="go_route(5)">5</button>
     <router-view></router-view>
   </div>
 </template>
@@ -14,6 +16,25 @@ export default {
         console.log(to, from, next)
         // 继续调用
         next()
+    },
+    methods: {
+        go_route (num) {
+            this.$router.push({ path: `/user/${num}` })
+
+            /*
+                // 字符串
+                router.push('home')
+
+                // 对象
+                router.push({ path: 'home' })
+
+                // 命名的路由
+                router.push({ name: 'user', params: { userId: 123 }})
+
+                // 带查询参数，变成 /register?plan=private
+                router.push({ path: 'register', query: { plan: 'private' }})
+            */
+        }
     }
 }
 </script>
