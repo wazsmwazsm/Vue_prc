2d3f7911ff72c453e9a71575dc099a9373a85a66
wazsmwazsm <18748406022@163.com>
2018-01-30 20:07:09 +0800

    add elementui sample

---
+++ elementUi/sample/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <el-button @click="visible = true">按钮</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>欢迎使用 Element</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return { visible: false }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+</style>
